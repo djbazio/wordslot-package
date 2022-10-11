3bac7033edca10c8b95c1c85f72e019192af7c46
Bazio <baziovvip@gmail.com>
2022-10-11 11:12:06 -0700

    First Upload

---
+++ spec/wordslot-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WordslotPackageView from '../lib/wordslot-package-view';
+
+describe('WordslotPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
